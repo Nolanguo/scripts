c7d54e1725fcb26f97577ab15b26c02d79f6501a
Nolan Guo <nolan.guo@cbsinteractive.com>
2015-01-18 20:13:22 -0800

    removing some files

--- generate_dns_records.sh
+++
@@ -1,9 +0,0 @@
-for ((i=1;i-16;i++)) 
-do
-	echo "`expr $i + 74`      IN      PTR     phx2-ccs-col-wax${i}.nguo.com."
-done
-
-for ((i=1;i-16;i++))
-do
-	echo "phx2-ccs-col-wax${i}	 IN      A       10.14.162.`expr $i + 74`"
-done
